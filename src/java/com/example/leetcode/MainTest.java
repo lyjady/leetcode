ee48398a7e48c3768b0889b768808916664d1b6b
991296755@qq.com <lyj112233>
2019-11-06 14:27:15 +0800

    Initial commit

---
+++ src/java/com/example/leetcode/MainTest.java
@@ -0,0 +1,15 @@
+package com.example.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author linyongjin
+ * @date 2019/11/6 14:13
+ */
+public class MainTest {
+
+    @Test
+    public void test1() {
+        String formula = "1/2+2/3";
+    }
+}
